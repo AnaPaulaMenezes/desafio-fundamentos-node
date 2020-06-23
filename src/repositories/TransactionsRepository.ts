715ebf7b32703424e5aba711f5e788a7e2cce0b3
Ana Paula Menezes <anapaularossi.577@gmail.com>
2020-06-23 20:33:44 -0300

    backend

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (
+      accumulator: Balance,
+      transaction: Transaction,
+    ): Balance => {
+      if (transaction.type === 'income') {
+        accumulator.income += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        accumulator.outcome += transaction.value;
+      }
+
+      accumulator.total = accumulator.income - accumulator.outcome;
+      return accumulator;
+    };
+    const balance = this.transactions.reduce(reducer, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
